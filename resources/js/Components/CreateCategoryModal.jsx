e62e4406088f2c5d7cae9b1583c6314badd8e2bb
XINUJ <159104110+XINUJ@users.noreply.github.com>
2024-11-18 16:51:17 +0800

    Modal integration
    + New Integrated Modals
    > CreateCategoryModal
    > EditCatModal
    + Fixed Modal C greenbtn

--- resources/js/Components/CreateCategoryModal.jsx
+++ resources/js/Components/CreateCategoryModal.jsx
@@ -2,36 +2,52 @@ import ModalB from "@/Layouts/ModalB";
 import MainInputField from '@/Components/MainInputField';
 import { Checkbox } from 'antd';
 import DropDownField from '@/Components/DropDownField';
+import { useForm } from '@inertiajs/react'
 
 function CreateCategoryModal({ type, isModalOpen, handleCancel}){
+
+    const { data, setData, post, errors, processing  } = useForm({
+        category_type: '',
+        custom_name: '',
+        def_cat: '',
+    });   
+
+    function submit(e) {
+        e.preventDefault();
+       console.log(data);
+    }
+
     return(
         <>
+         <form onSubmit={submit}>
             <ModalB
                 title="Create Category"
                 subtitle={ type && (type === "expense" ? "Expense" : "Income") }
                 isModalOpen={isModalOpen}
                 handleCancel={handleCancel}
+                onSubmit={submit}
             >
 
             <div className="space-y-4 > *">
                 <DropDownField
                     label="Select Category Type"
-                    htmlFor="category-type"
+                    htmlFor="category_type"
                     name="category-type"
                     placeholder="Select a Category"
                     options={[
-                        { label: 'Option 1', value: 'option1' },
-                        { label: 'Option 2', value: 'option2' },
-                        { label: 'Option 3', value: 'option3' }
+                        { label: 'Budget', value: 'budget' },
+                        { label: 'Expense', value: 'expense' },
                     ]}
+                    onChange={(e) => setData('category_type', e.target.value)}
                 />
 
                 <MainInputField 
                     label="Custom Name"
-                    htmlFor="custom-name"
+                    htmlFor="custom_name"
                     type="text"
                     name="custom-name"
                     placeholder="Enter Category Name"
+                    onChange={(e) => setData('custom_name', e.target.value)}
                 />
 
                     <Checkbox
@@ -43,7 +59,7 @@ function CreateCategoryModal({ type, isModalOpen, handleCancel}){
 
                 <DropDownField
                     label="Default Categories"
-                    htmlFor="def-cat"
+                    htmlFor="def_cat"
                     name="def-cat"
                     placeholder="Select a Category"
                     options={[
@@ -51,9 +67,13 @@ function CreateCategoryModal({ type, isModalOpen, handleCancel}){
                         { label: 'Option 2', value: 'option2' },
                         { label: 'Option 3', value: 'option3' }
                     ]}
+                    onChange={(e) => setData('def_cat', e.target.value)}
                 />
             </div>
+
             </ModalB>
+
+            </form>
         </>
     )
 }
--- resources/js/Components/EditCatModal.jsx
+++ resources/js/Components/EditCatModal.jsx
@@ -1,9 +1,21 @@
 import ModalC from "@/Layouts/ModalC";
 import MainInputField from '@/Components/MainInputField';
+import { useForm } from '@inertiajs/react'
 
 function EditCatModal({isModalOpen, handleCancel}){
+
+    const { data, setData } = useForm({
+        category_name: '',
+    });   
+
+    function submit(e) {
+        e.preventDefault();
+        console.log(data);
+    }
+
     return(
         <>
+        <form onSubmit={submit}>
             <ModalC 
                 title="Edit Category"
                 subtitle="Food & Drink"
@@ -11,18 +23,20 @@ function EditCatModal({isModalOpen, handleCancel}){
                 handleCancel={handleCancel}
                 deleteTitle="Category"
                 deleteContent="Are you sure you want to delete this category?"
+                onClick={submit}
             >
-
-                    <div className="space-y-5 > * mb-8">
-                        <MainInputField 
-                            label="Name"
-                            htmlFor="cat-name"
-                            type="text"
-                            name="cat-name"
-                            placeholder="Food & Drink"
-                        />
-                    </div>
+                <div className="space-y-5 > * mb-8">
+                    <MainInputField 
+                        label="Name"
+                        htmlFor="category_name"
+                        type="text"
+                        name="cat-name"
+                        placeholder="Food & Drink"
+                        onChange={(e) => setData('category_name', e.target.value)}
+                    />
+                </div>
             </ModalC>
+        </form>
         </>
     )
 }
