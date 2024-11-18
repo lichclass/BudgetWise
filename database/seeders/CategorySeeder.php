3eb97b43c80c8de0afebd2d29e060a2fc709300f
Hams <sam.agunod@gmail.com>
2024-11-18 15:17:31 +0800

    update category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Category;
+use App\Models\Categories;
 
 class CategorySeeder extends Seeder
 {
