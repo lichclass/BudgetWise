80c7666f3ea76fbddb1c18fb50d0304e18473908
XINUJ <159104110+XINUJ@users.noreply.github.com>
2024-11-27 17:27:07 +0800

    New Page for Admin
    + New Dashboard Page

--- resources/js/Pages/Admin/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -8,6 +8,45 @@ function Dashboard() {
             <Head title="Dashboard" />
 
             <Main navbarMsg={'Welcome back, Admin'} isAdmin={true}> 
+
+                <div
+                    className="flex flex-row shadow-xl h-48 w-full justify-evenly rounded-xl text-white border border-white/40 p-6 pb-8
+                            hover:border-white/80 transition-all duration-300 pr-20"
+                    style={{
+                        background:
+                            "linear-gradient(259deg, rgba(74, 167, 200, 0.12) -9.8%, rgba(13, 33, 47, 0.12) 119.45%)",
+                    }}
+                >
+                    <div className="flex flex-col text-center w-36">
+                        <h1 className="font-extrabold text-3xl text-gray-400">Users</h1>
+                        <h1 className="font-extrabold text-5xl">10</h1>
+                        <h1>10% increase from last month</h1>
+                    </div>
+
+                    <div className="flex flex-col text-center w-36">
+                        <h1 className="font-extrabold text-3xl text-gray-400">Visits</h1>
+                        <h1 className="font-extrabold text-5xl">10</h1>
+                        <h1>10% increase from last month</h1>
+                    </div>
+
+                    <div className="flex flex-col text-center w-36 pt-3">
+                        <h1 className="font-extrabold text-3xl text-gray-400">Transactions</h1>
+                        <h1 className="font-extrabold text-5xl pl-9">300</h1>
+                    </div>
+
+                </div>
+
+                <div
+                    className="shadow-xl w-full rounded-xl text-white border border-white/40 p-6 pb-8 space-y-2 mt-8
+                            hover:border-white/80 transition-all duration-300"
+                    style={{
+                        background:
+                            "linear-gradient(259deg, rgba(74, 167, 200, 0.12) -9.8%, rgba(13, 33, 47, 0.12) 119.45%)",
+                    }}
+                >
+                            <h1>Graph Here...</h1>
+                </div>
+
             </Main>
         </>
 
