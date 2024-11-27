f09aac5c0156d21d9dc025f2d4300c1281294c1d
Nash Adam Munoz <150409949+lichclass@users.noreply.github.com>
2024-11-27 16:17:10 +0800

    Update CreateCategoryModal.jsx

--- resources/js/Components/CreateCategoryModal.jsx
+++ resources/js/Components/CreateCategoryModal.jsx
@@ -2,14 +2,34 @@ import ModalB from "@/Layouts/ModalB";
 import MainInputField from "@/Components/MainInputField";
 import { Checkbox } from "antd";
 import DropDownField from "@/Components/DropDownField";
-import { useForm } from "@inertiajs/react";
+import { useState } from "react";
+import { useForm, usePage } from "@inertiajs/react";
 
 function CreateCategoryModal({ type, isModalOpen, handleCancel }) {
-    const { setData, post, errors, processing } = useForm({
+    const { default_cat, categories } = usePage().props;
+    const [isCustom, setIsCustom] = useState(false);
+
+    const filteredDefaultCat = default_cat.filter(
+        (cat) => 
+            cat.category_type === type && 
+            !categories.some((existingCat) => existingCat.category_id === cat.category_id) 
+    );
+
+    const { data, setData, post, errors, processing } = useForm({
         category_type: type,
         custom_name: "",
-        def_cat: "",
+        def_cat: filteredDefaultCat[0].category_id,
     });
+
+    const handleIsCustomChange = (e) => { 
+        setIsCustom(e.target.checked); 
+        if(e.target.checked) {
+            setData("def_cat", "");
+        } else {
+            setData("custom_name", "");
+            setData("def_cat", filteredDefaultCat[0].category_id);
+        }
+    };
 
     function submit(e) {
         e.preventDefault();
@@ -30,6 +50,7 @@ function CreateCategoryModal({ type, isModalOpen, handleCancel }) {
                     htmlFor="category_type"
                     name="category-type"
                     placeholder="Select a Category"
+                    value={type}
                     options={[
                         { label: "Income", value: "income" },
                         { label: "Expense", value: "expense" },
@@ -44,11 +65,13 @@ function CreateCategoryModal({ type, isModalOpen, handleCancel }) {
                     name="custom-name"
                     placeholder="Enter Category Name"
                     onChange={(e) => setData("custom_name", e.target.value)}
+                    isReadOnly={!isCustom}
                 />
 
                 <Checkbox
                     name="custom-cat"
                     className="text-md font-extralight text-white"
+                    onChange={handleIsCustomChange}
                 >
                     Custom Category
                 </Checkbox>
@@ -58,12 +81,13 @@ function CreateCategoryModal({ type, isModalOpen, handleCancel }) {
                     htmlFor="def_cat"
                     name="def-cat"
                     placeholder="Select a Category"
-                    options={[
-                        { label: "Option 1", value: "option1" },
-                        { label: "Option 2", value: "option2" },
-                        { label: "Option 3", value: "option3" },
-                    ]}
+                    value={data.def_cat}
+                    options={filteredDefaultCat.map((cat) => ({
+                        label: cat.category_name,
+                        value: cat.category_id,
+                    }))}
                     onChange={(e) => setData("def_cat", e.target.value)}
+                    isReadOnly={isCustom}
                 />
             </div>
         </ModalB>
