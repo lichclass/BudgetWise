c66022f5b056368264dd5581e56bc51524cb4658
Sharly Barago <sharlybarago18@gmail.com>
2024-12-08 13:53:25 +0800

    Expenses Progress Bar
    o Finished the progress bar for the add expenses modal within the category cards

--- resources/js/Components/ExpenseProgressBar.jsx
+++ resources/js/Components/ExpenseProgressBar.jsx
@@ -1,13 +1,15 @@
-function ExpenseProgressBar ({completion = 0}){
+function ExpenseProgressBar({ completion = 0 }) {
+    
+    completion = completion.toFixed(2);
 
     return (
-        <div className="w-full h-6 rounded-full bg-[#0A1C29] bg-opacity-25 flex items-center">
-            <div className={`h-6 bg-blue-600 ${
-                completion < 100 ? "rounded-l-full" : "rounded-full"
-                    } bg-gradient-to-r from-[#79BAA8] to-[#195676]`}
+        <div className="w-full h-12 rounded-lg bg-[#37474F] bg-opacity-25 flex items-center">
+            <div className={`h-12 flex items-center ${completion < 100 ? "bg-[#2E7C99]" : "bg-[#9E4242]"} ${
+                completion < 100 ? "rounded-l-lg" : "rounded-full"
+                    }`}
                     style={{ width: `${completion}%` }}
             >
-                <h1 className="">{completion}%</h1>
+                <h1 className="text-white whitespace-nowrap px-5 font-semibold">{completion}% {completion > 100 ? "Exceeding" : "Spent"}</h1>
             </div>
         </div>
     );
